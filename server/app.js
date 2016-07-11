7e2e1806f714000222750474a9620264bb542b56
Tracey Zavadil <shinybirdy@yahoo.com>
2016-07-11 02:33:14 -0500

    added templates and reconfig index.html

--- server/app.js
+++ server/app.js
@@ -1,20 +1,46 @@
 //server side
+console.log("Hey - from app.js");
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
 var path = require('path');
-var connectionString = 'postgres://localhost:5432/music_studio_tracker';
+//var router = express.Router();
+var pg = require('pg');
+//connect to SQL database "music_studio_tracker"
+var connectionString = "postgres://localhost:5432/music_studio_tracker";
+// connect to passport
+//var passport = require('.')
 
-app.set("port",(process.env.PORT || 5000));
+//Route inclusion
+//var login = require("./routes/login");
+//var register = require("./routes/register");
+//var router = require('./routes/router');
 
-app.listen(app.get("port"), function(){
+//body-parser middleware
+app.use(bodyParser.json());
+
+//server
+app.set("port",(process.env.PORT||5000));
+app.listen( app.get("port"), function(){
   console.log("Server is listening on port 5000, darling...");
 });
+
 app.get('/*',function(req,res){
-  var file = req.params[0] || "views/index.html";
-  res.sendFile(path.join(__dirname,"/public/",file));
+  console.log("at base url, so that's something...");
+  var file= req.params[0]||"/views/index.html";
+  res.sendFile(path.join(__dirname,"/public", file));
 });
 
+
+//set static page
 app.use(express.static('public'));
 
-module.exports=app;
+//app.use('/', router);
+
+//set base url
+app.get('/*',function(req,res){
+  console.log("at base url, so that's something...");
+  res.sendFile(path.resolve("./views/index.html"));
+});
--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,5 +1,36 @@
 console.log("Hi from client app!");
+
 var myApp = angular.module('myApp',[]);
+
+myApp.config(["$routeProvider", function($routeProvider){
+  $routeProvider.
+    when("/home", {
+      templateUrl:"/views/routes/home.html",
+      controller:"HomeController"
+    }).
+    when("/master_schedule", {
+      templateUrl:"/views/routes/master_schedule.html"
+    }).
+    when("/master_student_list", {
+      templateUrl:"/views/routes/master_student_list.html"
+    }).
+    when("/add_student", {
+      templateUrl:"/views/routes/add_student.html"
+    }).
+    otherwise({
+      redirectTo:"home"
+    });
+
+}]);
+
+
+myApp.controller("HomeController", ["$scope",function($scope){
+  console.log("Loaded Home");
+  $scope.counter =1;
+  $scope.iterate=function(){
+    $scope.counter++;
+  };
+}]);
 
 myApp.controller("logInController", ["$scope", "$http", function($scope, $http){
 
